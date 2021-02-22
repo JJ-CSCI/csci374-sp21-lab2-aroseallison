9159bbd4d560f3a1780112d9af575041b15fa6f6
Rose Allison <arose.allison@gmail.com>
2021-02-22 23:01:51 +0000

    Tried to change the functions

--- main.fs
+++ main.fs
@@ -3,11 +3,17 @@ module Assignment
 type AMPM = AM | PM
 
 // This function checks if an hour value `h` is not in [1,12] range
-let areHoursInvalid h =
+let areHoursInvalid h = 
+  if h < 1|| h > 12 then 
+    true
+  else 
     false
 
 // This function checks if a minute value `m` is not in [0,59] range
 let areMinutesInvalid m =
+  if m < 0 || m > 59 then
+    true 
+  else
     false
 
 // This function creates a valid time tuple
@@ -17,4 +23,7 @@ let time h m ampm :(int * int * AMPM) =
 
 // This function compares two times in tuple format
 let lessThan (time1: int * int * AMPM) (time2: int * int * AMPM) :bool =
+  if time1 > time2 || time1 = time2 then
     false
+  else true 
+
